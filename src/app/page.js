7820acb8a65106f21aa7bf0da5ce494781367051
Omar_09 <136336035+omarker09@users.noreply.github.com>
2023-12-04 15:06:48 +0100

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,7 @@ import Image from 'next/image'
 import ViPalme from "../../public/GARanHiXUAA9Xbx.jpg"
 import Timer from './componnent/timer'
 export default function Home() {
-  const endDate = new Date('2023-12-05T15:00:00');
+  const endDate = new Date('2023-12-05T14:00:00');
   const Rockstarlink = "https://www.youtube.com/@RockstarGames"
   return (
     <div style={{height: "100dvh"}} className=' main h-full bg-slate-100 w-full'>
